f81ada292d6c47d21799865fe4f9cb57c181db0e
Aniket9977 <aniketvishwakarma459@gmail.com>
2024-08-10 22:56:56 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
 from dotenv import load_dotenv
 
 load_dotenv()
-
+import base64
 import streamlit as st
-
 import os
-from PIL import Image
+import io
+from PIL import Image 
 import pdf2image
 import google.generativeai as genai
 
-genai.configure(api_key=os.getenv('google_api_key'))
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-
-def get_gemini_response(input , pdf_content , prompt):
-    model = genai.GenerativeModel('gemini-pro-vision')
-    response = model.generate_content([input , pdf_content[0] , prompt])
+def get_gemini_response(input,pdf_cotent,prompt):
+    model=genai.GenerativeModel('gemini-pro-vision')
+    response=model.generate_content([input,pdf_content[0],prompt])
     return response.text
 
 def input_pdf_setup(uploaded_file):
     if uploaded_file is not None:
-        images = pdf2image.convert_from_bytes(uploaded_file.read())
-        first_page = images[0]
+        ## Convert the PDF to image
+        images=pdf2image.convert_from_bytes(uploaded_file.read())
+
+        first_page=images[0]
+
+        # Convert to bytes
         img_byte_arr = io.BytesIO()
         first_page.save(img_byte_arr, format='JPEG')
         img_byte_arr = img_byte_arr.getvalue()
@@ -34,7 +37,9 @@ def input_pdf_setup(uploaded_file):
         return pdf_parts
     else:
         raise FileNotFoundError("No file uploaded")
-    
+
+## Streamlit App
+
 st.set_page_config(page_title="ATS Resume EXpert")
 st.header("ATS Tracking System")
 input_text=st.text_area("Job Description: ",key="input")
@@ -80,3 +85,11 @@ elif submit3:
         st.write(response)
     else:
         st.write("Please uplaod the resume")
+
+
+
+   
+
+
+
+
